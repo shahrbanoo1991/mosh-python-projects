52dd89b0b04b49b68f94b27a5a535ba7b0106a85
ms.zaboli <lady1991mo@gmail.com>
2026-08-02 20:46:21 +0100

    Complete dice rolling game

--- DiceRollingGame/DiceRollingGame.py
+++ DiceRollingGame/DiceRollingGame.py
@@ -1,1 +1,11 @@
-
+import random
+a = [1, 2, 3, 4, 5, 6]
+choice = input("Roll the dice? (y.n): ") 
+if choice.lower() == 'a':
+    print('Invalid choice')
+elif choice.lower() == 'b':
+    print('Invalid choice')
+elif choice.lower() == 'y':
+  print(random.choice(a))
+else:
+    print('Thanks for playing')
